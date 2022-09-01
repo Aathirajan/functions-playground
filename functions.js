0cc305d9bc485693b25d6ff4ccf884b04b88f6bf
Aathi Rajan S <99598685+Aathirajan@users.noreply.github.com>
2022-09-01 12:42:33 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,46 @@
+// // template literls
+// function Greeting(Name)
+// {
+//     console.log(`hi ${Name} How was your day`)
+// }
+
+// Greeting('Aathi');
+// if something is wrapped inside the dollor quotes its an value for javascript
+
+
+//Return statement
+
+function sum(a, b) {
+    return (a + b)
+}
+addition =sum(1, 2);
+console.log(addition);
+
+// // Tip calculator
+
+// function totalprice(food,tip)
+// {
+//    foodpercent=
+
+// }
+
+
+//arrow functions 
+const sum =(a,b)=>a+b
+sum(10,20); 
+
+// Excersice:
+
+
+const add = (a, b) => a + b
+console.log(add(10, 20));
+
+
+const sub = (c, d) => c - d
+console.log(sub(50, 20));
+
+const division = (e, f) => e / f
+console.log(division(90, 3))
+
+const mul = (g, h) => g * h
+console.log(mul(5, 6));
